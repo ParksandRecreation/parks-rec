ccb7d5dde337f139cf2df601c05a9cf423c3d4dd
Jeffrey Huang <jeffuhuang@yahoo.com>
2023-06-06 09:45:10 -0700

    add global error handler and serve static files

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import { Request, Response, NextFunction } from 'express';
+import path from 'path';
 
 const app = express();
 const server = createServer(app);
@@ -16,10 +18,25 @@ const PORT: number = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// serve static files
+app.use(express.static(path.join(__dirname, '../dist')));
 
+// serve index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../dist/index.html'));
+});
 
-// express global error handler
-
+// express global error handler (use any for error type until we define custom error type later)
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign(defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
 
 server.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);
