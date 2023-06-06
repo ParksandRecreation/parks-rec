fbe9f015a999cba71c2f3840166d8de43e82d78e
Rathna Chinthalapalli <rathnac@rathnas-mbp.lan>
2023-06-05 21:07:33 -0700

    refactor: added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import  LoginPage from './components/LoginPage';
+import  HomePage from './components/HomePage';
 
 const App = () => {
   return (
-   <Router>
+   <BrowserRouter>
       <Routes>
-        <Route path='/home' element={<HomePage />}></Route>
-        <Route path="/" element={<LoginPage />}></Route>
+         <Route path='/home' element={<HomePage />}></Route>
+         <Route path="/" element={<LoginPage />}/>
+        
       </Routes>
-   </Router>
+    </BrowserRouter>
+  
   );
 };
 
