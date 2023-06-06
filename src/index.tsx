4fb526ca9c1b7254f26a5a7b32b3710fb5d76cd2
Rathna Chinthalapalli <rathnac@rathnas-mbp.lan>
2023-06-05 21:09:24 -0700

    fix: fixed App module error

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
 import App from './App';
+
+// const GOOGLE_CLIENT_ID = '';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
 root.render(
-    <BrowserRouter>
-        <App />     
-    </BrowserRouter>
+    // <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+  
+        <App />
+    /* </GoogleOAuthProvider> */
 );
