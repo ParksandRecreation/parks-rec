05adf0b9beb797bfc3cf6d74c7c73e3578946951
Lilianne Mueller <lilyanneom@gmail.com>
2023-06-06 10:16:50 -0700

    sql table edits

--- create_table.sql
+++ create_table.sql
@@ -1,24 +1,27 @@
 
 --user table with PK ID
-CREATE TABLE USER (
-"_id" SERIAL NOT NULL,
+CREATE TABLE users (
+"_id" PRIMARY KEY,
 "oauth_id" VARCHAR NOT NULL,
 "username" VARCHAR NOT NULL
-)
+);
 --scores table with highest score and associated user_Id
 CREATE TABLE scores (
     "highest_score" INTEGER NOT NULL,
     "user_id" INTEGER
     FOREIGN KEY (user_id) REFERENCES user(user_id)
-)
+);
 
 CREATE TABLE parks (
-    "_id" SERIAL NOT NULL,
+    "_id" PRIMARY KEY,
     "name" VARCHAR NOT NULL,
     "img_url" VARCHAR NOT NULL
-)
+);
 
 CREATE TABLE faves (
-    "user_id",
-    "park_id"
-)
+    "user_id" INTEGER,
+    "park_id" INTEGER,
+    FOREIGN KEY ("user_id") REFERENCES users("_id"),
+    FOREIGN KEY ("park_id") REFERENCES parks("_id"),
+    PRIMARY KEY ("user_id", "park_id")
+);
