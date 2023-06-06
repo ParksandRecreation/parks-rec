407bf448504834f07470112b20ba49f2599909a6
Lilianne Mueller <lilyanneom@gmail.com>
2023-06-06 11:48:28 -0700

    tables created, pool created, usercontroller

--- create_table.sql
+++ create_table.sql
@@ -1,26 +1,24 @@
-
 --user table with PK ID
 CREATE TABLE users (
-"user_id" SERIAL PRIMARY KEY NOT NULL,
-"oauth_id" VARCHAR NOT NULL,
-"username" VARCHAR NOT NULL
+"user_id" VARCHAR PRIMARY KEY,
+"username" VARCHAR UNIQUE NOT NULL
 );
 
 --scores table with highest score and associated user_Id
 CREATE TABLE scores (
     "highest_score" INTEGER NOT NULL,
-    "user_id" INTEGER, 
+    "user_id" VARCHAR, 
     FOREIGN KEY ("user_id") REFERENCES users("user_id")
 );
 
 CREATE TABLE parks (
-    "park_id" SERIAL PRIMARY KEY NOT NULL,
+    "park_id" SERIAL PRIMARY KEY,
     "name" VARCHAR NOT NULL,
     "img_url" VARCHAR NOT NULL
 );
 
 CREATE TABLE faves (
-    "user_id" INTEGER,
+    "user_id" VARCHAR,
     "park_id" INTEGER,
     FOREIGN KEY ("user_id") REFERENCES users("user_id"),
     FOREIGN KEY ("park_id") REFERENCES parks("park_id")
