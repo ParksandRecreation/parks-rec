19328f4fbc472aef02bb81542d5095337e71d5ee
Rathna Chinthalapalli <rathnac@rathnas-mbp.lan>
2023-06-06 14:31:24 -0700

    refactor: add image and css link

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,24 +1,33 @@
 
 import { useGoogleLogin } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
+import tree from '../assets/tree.png';
 
 const LoginPage = () => {
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
 
-    // const loginToGoogle = useGoogleLogin({
+  const loginToGoogle = () => {
     //     onSuccess: tokenResponse => {
     //         localStorage.setItem('loginWith', 'Github')
     //         localStorage.setItem('accessToken', tokenResponse.access_token)
-    //         navigate('/home')
-    //     },
-    // })
+    //     navigate('/home')
+    //   },
+    navigate('/home');
+    }
 
     return(
-    <div className='loginContainer'>
-        <h1>Login with Google</h1>
-         <button > Login</button>
-    </div>
-        
+        <>
+        <nav className="Navbar-login">          
+            <div className="navbar-header ">
+                <h1>Parks & Rec</h1>
+            </div>          
+        </nav>
+        <div className='loginContainer'>
+            <h1>Login with Google</h1>
+            <button onClick={() => loginToGoogle()}> Login</button>
+        </div>
+        <img className="tree" src={tree}/>
+        </>
     )
 }
 
