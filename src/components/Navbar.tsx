92fc818caab177553ce2b5b2de2c3f1cb0c265b1
Rathna Chinthalapalli <rathnac@rathnas-mbp.lan>
2023-06-06 14:30:02 -0700

    feat: include navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  //links to other routes in react router dom
+  return (<div className='Navbar'>
+    <Link to='/home'>
+      Home
+    </Link>
+    <Link to='/'>
+      Log Out
+    </Link>
+  </div>
+  )
+}
+
+export default Navbar;
