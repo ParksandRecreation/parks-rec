137e6fcc71fc99c8fefbb096c13af54e6f968342
Ruxin Zheng <RuxinZ@users.noreply.github.com>
2023-06-06 07:31:06 -0700

    set up oAuth on front end

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './components/App';
 
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 const container = document.getElementById('app-root')!;
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId="143680018728-1fb574qfkojddts9msssul16a7jo6kjc.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+);
