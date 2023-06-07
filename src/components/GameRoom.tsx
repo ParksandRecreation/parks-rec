68a63dea45f82c28d940658f75397b019a7ccc50
Rathna Chinthalapalli <hellorathnac@gmail.com>
2023-06-06 20:08:41 -0700

    refactor: add playercontainer and styles for gameroom

--- src/components/GameRoom.tsx
+++ src/components/GameRoom.tsx
@@ -1,6 +1,8 @@
 import Navbar from './Navbar';
 import tree from '../assets/tree.png';
 import camper from '../assets/camper.png';
+import sample from '../assets/sample.jpg';
+
 
 const GameRoom = () => {
 
@@ -9,10 +11,15 @@ return(
     <>
 
      <Navbar />
-     <div className="gameRoomContainer">
+     <div className = "mainContainer">
+    <div className="counter">
+        <span>00:00</span>
+    </div>
+      <div className="gameRoomContainer">
         <h2>Guess the National Park...</h2>
         <div className="imageContainer">
-
+        {/* Insert Image here */}
+        <img className="park_image" src={sample}/>
         </div>
         <div className = "btnContainer">
             <button>Yellowstone National Park</button>
@@ -22,7 +29,21 @@ return(
             <button>Yosemite National Park</button>
             <button>Grand Canyon National Park</button>
         </div>
-    </div>
+      </div>
+      <div className = 'playerMainContainer'>
+        <div className='playerContainer'>
+        <span>Player 1</span>
+        <span className='score1'>100</span>
+        </div>
+        <div className='playerContainer'>
+        <span>Player 2</span>
+        <span className='score2'>200</span>
+        </div>
+      </div>
+
+
+     </div>
+     
     <img className="gr_img tree1" src={tree}/>
     <img className="gr_img tree2" src={tree}/>
     <img className="gr_img" src={tree}/>
