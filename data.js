b2069dd325712ef52ba9a7b3f47da27377e60c85
Lilianne Mueller <lilyanneom@gmail.com>
2023-06-06 18:46:38 -0700

    data file complete

--- data.js
+++ data.js
@@ -1,66 +1,8 @@
-import axios from 'axios';
+// import axios from 'axios';
+const axios = require('axios');
+const fs = require('fs');
 
-const parks = [
-  'ACAD',
-  'ARCH',
-  'BADL',
-  'BIBE',
-  'BISC',
-  'BLCA',
-  'BRCA',
-  'CANY',
-  'CARE',
-  'CAVE',
-  'CHIS',
-  'CONG',
-  'CRLA',
-  'CUVA',
-  'DENA',
-  'DEVA',
-  'DRTO',
-  'EVER',
-  'GAAR',
-  'GLAC',
-  'GLBA',
-  'GRBA',
-  'GRCA',
-  'GRSA',
-  'GRSM',
-  'GRTE',
-  'GUMO',
-  'HALE',
-  'HAVO',
-  'HOSP',
-  'ISRO',
-  'JOTR',
-  'KATM',
-  'KEFJ',
-  'KOVA',
-  'LACL',
-  'LAVO',
-  'MACA',
-  'MEVE',
-  'MORA',
-  'NOCA',
-  'NPSA',
-  'OLYM',
-  'PEFO',
-  'PINN',
-  'REDW',
-  'ROMO',
-  'SAGU',
-  'SEKI',
-  'SHEN',
-  'THRO',
-  'VOYA',
-  'WICA',
-  'WRST',
-  'WTNP',
-  'YELL',
-  'YOSE',
-  'YUCH',
-  'ZION',
-];
+const parksArray = ['YELL', 'YOSE', 'YUCH', 'ZION'];
 
 // const fetchParkData = async (parkCodes) => {
 //   try {
@@ -79,23 +21,148 @@ const parks = [
 
 // console.log(fetchParkData(parks));
 
-const fetchParkData = async (parkCode) => {
+const fetchParkData = async () => {
+  const parksInfo = [];
   try {
-    // const url = 'https://developer.nps.gov/api/v1/parks?';
-    const park = 'Arches';
-    const apiKey = 'IWyuEZvrGxb378F1joFqwX1BI0KPDGpLeqvAi87D&limit=100';
-    const response = axios.get(
-      `https://developer.nps.gov/api/v1/parks/${parkCode}?api_key=${apiKey}&q=${park}`
-    );
-    const parkData = response.data;
-    const description = parkData[0].description;
-    // const imageUrls = parkData.images.map((image) => image.url);
+    const result = {};
+    const urls = [];
+    const parkCode = parksArray;
+    const url = 'https://developer.nps.gov/api/v1/parks?';
+    const apiKey = 'IWyuEZvrGxb378F1joFqwX1BI0KPDGpLeqvAi87D';
+    //https://developer.nps.gov/api/v1/parks?api_key=IWyuEZvrGxb378F1joFqwX1BI0KPDGpLeqvAi87D&parkCode=yose&limit=10
 
-    console.log(description);
-    // console.log(imageUrls);
+    const response = await axios.get(
+      `${url}api_key=${apiKey}&parkCode=${parkCode[1]}`
+    );
+    const parkData = response.data.data;
+    const fullName = parkData[0].fullName;
+    const images = parkData[0].images;
+
+    images.forEach((image) => {
+      urls.push(image.url);
+    });
+    result.parkName = fullName;
+    result.images = urls;
+    parksInfo.push(result);
+
+    if (parksInfo.length === parksArray.length) {
+      writeToFile(JSON.stringify(parksInfo, null, 2), 'parkData.txt');
+    }
+    console.log('parksInfo', parksInfo);
   } catch (err) {
     console.log(`Error in fetch: ${err}`);
   }
 };
-const parkCode = 'ACAD'; // Replace with the desired park code
-fetchParkData(parkCode);
+
+//fetchParkData();
+
+// const writeToFile = (data, fileName) => {
+//   fs.writeFile(fileName, data, (err) => {
+//     if (err) {
+//       console.error('Error writing to file:', err);
+//     } else {
+//       console.log(`Data written to ${fileName} successfully.`);
+//     }
+//   });
+// };
+
+/////////////////
+const getParkData = async () => {
+  try {
+    const parks = parksArray[0];
+    const result = {};
+    const url = 'https://developer.nps.gov/api/v1/parks?';
+    const apiKey = 'IWyuEZvrGxb378F1joFqwX1BI0KPDGpLeqvAi87D'; // Replace with your NPS API key
+
+    const promises = parks.map(async (parkCode) => {
+      const response = await axios.get(
+        `${url}api_key=${apiKey}&?parkCode=${parkCode}`
+      );
+
+      const parkData = response.data.data;
+      const fullName = parkData[0].fullName;
+      const images = parkData[0].images;
+
+      const urls = images.map((image) => image.url);
+
+      result[parkCode] = {
+        parkName: fullName,
+        images: urls,
+      };
+    });
+
+    await Promise.all(promises);
+
+    writeToFile(JSON.stringify(result, null, 2), 'parkData.txt');
+    console.log(result);
+  } catch (err) {
+    console.log(`Error in fetch: ${err}`);
+  }
+};
+
+// const writeToFile = (data, fileName) => {
+//   fs.writeFile(fileName, data, (err) => {
+//     if (err) {
+//       console.error('Error writing to file:', err);
+//     } else {
+//       console.log(`Data written to ${fileName} successfully.`);
+//     }
+//   });
+// };
+
+//getParkData();
+
+////
+const axiosFetchParkData = async () => {
+  const parksNameAndUrl = [];
+  let index = 0;
+
+  const intervalId = setInterval(async () => {
+    try {
+      const result = {};
+      const urls = [];
+      const parkCode = parksArray[index];
+      const url = 'https://developer.nps.gov/api/v1/parks?';
+      const apiKey = 'IWyuEZvrGxb378F1joFqwX1BI0KPDGpLeqvAi87D';
+
+      const response = await axios.get(
+        `${url}api_key=${apiKey}&parkCode=${parkCode}`
+      );
+      const parkData = response.data.data;
+      const fullName = parkData[0].fullName;
+      const images = parkData[0].images;
+
+      images.forEach((image) => {
+        urls.push(image.url);
+      });
+      result.parkName = fullName;
+      result.images = urls;
+      parksNameAndUrl.push(result);
+      console.log('parkdataArr', parksNameAndUrl);
+
+      if (parksNameAndUrl.length === parksArray.length) {
+        clearInterval(intervalId);
+        writeToFile(JSON.stringify(parksNameAndUrl, null, 2), 'parkData.txt');
+      }
+
+      index++; // Increment the index
+      if (index >= parksArray.length) {
+        clearInterval(intervalId);
+      }
+    } catch (err) {
+      console.log(`Error in fetch: ${err}`);
+    }
+  }, 5000);
+};
+
+axiosFetchParkData();
+
+const writeToFile = (data, fileName) => {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+    } else {
+      console.log(`Data written to ${fileName} successfully.`);
+    }
+  });
+};
